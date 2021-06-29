0fcd4cb66cb287b29dc85ba8e340a570bdcc7e36
NataliaHivelocity <natalia@hivelocity.co.jp>
2021-06-29 19:08:04 +0900

    Edited and tested credit card controller

--- apps/hub_payments/lib/hub_payments_web/controllers/api/v1/credit_card_controller.ex
+++ apps/hub_payments/lib/hub_payments_web/controllers/api/v1/credit_card_controller.ex
@@ -4,10 +4,34 @@ defmodule HubPaymentsWeb.Api.V1.CreditCardController do
   alias HubPayments.Wallets
   alias HubPayments.Wallets.Wallet
 
+  def index(conn, %{"wallet_uuid" => wallet_uuid}) do
+    credit_cards = Wallets.list_credit_cards(%{wallet_uuid: wallet_uuid})
+    render(conn, "index.json", credit_cards: credit_cards)
+  end
+
+  def show(conn, %{"wallet_uuid" => wallet_uuid, "credit_card_uuid" => credit_card_uuid}) do
+    with credit_card = Wallets.get_credit_card(%{uuid: credit_card_uuid, wallet_uuid: wallet_uuid}) do
+    render(conn, "show.json", %{credit_card: credit_card})
+    end
+  end
+
   def create(conn, %{"uuid" => uuid, "credit_card" => credit_card_params}) do
     with %Wallet{id: wallet_id} <- Wallets.get_wallet(%{uuid: uuid}),
          {:ok, credit_card} <- Wallets.create_credit_card(credit_card_params, wallet_id) do
       render(conn, "show.json", %{credit_card: credit_card})
+    end
+  end
+
+  def update(conn, %{"wallet_uuid" => wallet_uuid, "credit_card_uuid" => credit_card_uuid, "credit_card_params" => credit_card_params}) do
+    with credit_card = Wallets.get_credit_card(%{uuid: credit_card_uuid, wallet_uuid: wallet_uuid}),
+         {:ok, credit_card} <- Wallets.update_credit_card(credit_card, credit_card_params) do
+      render(conn, "show.json", %{credit_card: credit_card})
+    end
+  end
+
+  def delete(conn, %{"credit_card" => credit_card}) do
+    with {:ok, _} <- Wallets.delete_credit_card(credit_card) do
+      send_resp(conn, :no_content, "")
     end
   end
 end
---
+++ apps/hub_payments/test/hub_payments_web/controllers/api/v1/credit_card_controller_test.exs
@@ -0,0 +1,141 @@
+defmodule HubPaymentsWeb.Api.V1.CreditCardControllerTest do
+  use HubPaymentsWeb.ConnCase
+
+  alias HubPayments.Wallets
+  alias HubPayments.Wallets.CreditCard
+
+  @create_attrs %{
+    brand: "some brand",
+    exp_month: "01",
+    exp_year: "23",
+    fingerprint: "some fingerprint",
+    last_four: "4321",
+    uuid: "some uuid"
+  }
+  @update_attrs %{
+    exp_month: "12",
+    exp_year: "32",
+  }
+  @invalid_attrs %{brand: nil, exp_month: nil, exp_year: nil, fingerprint: nil, last_four: nil, uuid: nil}
+
+  setup %{conn: conn} do
+    {:ok, conn:
+    build_conn()
+    |> put_req_header("accept", "application/json")
+    |> put_req_header("x-api-key", HubIdentity.Factory.insert(:api_key, type: "private").data)}
+  end
+
+  describe "index" do
+    test "lists all credit_cards", %{conn: conn} do
+      credit_card = insert(:credit_card)
+      insert(:credit_card)
+
+      [response] =
+        get(conn, "/api/v1/wallets/#{credit_card.wallet.uuid}/credit_cards")
+        |> json_response(200)
+      assert response["uuid"] == credit_card.uuid
+    end
+
+    test "returns [] if there are no credit cards", %{conn: conn} do
+      wallet = insert(:wallet)
+      response =
+        get(conn, "/api/v1/wallets/#{wallet.uuid}/credit_cards")
+        |> json_response(200)
+      assert response == []
+    end
+  end
+
+  describe "show credit_card" do
+    test "returns credit_card", %{conn: conn} do
+      wallet = insert(:wallet)
+      credit_card = insert(:credit_card, %{wallet: wallet})
+
+      response =
+        get(conn, "/api/v1/wallets/#{wallet.uuid}/credit_cards/#{credit_card.uuid}", %{
+          "wallet_uuid" => wallet.uuid,
+          "credit_card_uuid" => credit_card.uuid} )
+        |> json_response(200)
+
+
+      assert response["Object"] ==  "CreditCard"
+      assert response["uuid"] ==  credit_card.uuid
+      assert response["last_four"] ==  credit_card.last_four
+      assert response["exp_month"] ==  credit_card.exp_month
+      assert response["exp_year"] ==  credit_card.exp_year
+    end
+  end
+
+  describe "create credit_card" do
+    test "renders credit_card when data is valid", %{conn: conn} do
+      wallet = insert(:wallet)
+      response =
+        post(conn, "/api/v1/wallets/#{wallet.uuid}/credit_cards", %{
+          "uuid" => wallet.uuid,
+          "credit_card" => @create_attrs } )
+        |> json_response(200)
+
+      assert response["Object"] ==  "CreditCard"
+      assert response["last_four"] ==  @create_attrs.last_four
+      assert response["exp_month"] ==  @create_attrs.exp_month
+      assert response["exp_year"] ==  @create_attrs.exp_year
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      wallet = insert(:wallet)
+      response =
+        post(conn, "/api/v1/wallets/#{wallet.uuid}/credit_cards", %{
+          "uuid" => wallet.uuid,
+          "credit_card" => @invalid_attrs } )
+        |> json_response(400)
+
+      assert response["error"] != %{}
+    end
+  end
+
+  describe "update credit_card" do
+    test "renders credit_card when data is valid", %{conn: conn} do
+      wallet = insert(:wallet)
+      credit_card = insert(:credit_card, %{wallet: wallet})
+
+      response =
+        put(conn, "/api/v1/wallets/#{wallet.uuid}/credit_cards/#{credit_card.uuid}", %{
+          "wallet_uuid" => wallet.uuid,
+          "credit_card_uuid" => credit_card.uuid,
+          "credit_card_params" => @update_attrs} )
+        |> json_response(200)
+
+
+      assert response["Object"] ==  "CreditCard"
+      assert response["uuid"] ==  credit_card.uuid
+      assert response["last_four"] ==  credit_card.last_four
+      assert response["exp_month"] ==  "12"
+      assert response["exp_year"] ==  "32"
+    end
+
+    test "renders errors when data is invalid", %{conn: conn} do
+      wallet = insert(:wallet)
+      credit_card = insert(:credit_card, %{wallet: wallet})
+
+      response =
+        put(conn, "/api/v1/wallets/#{wallet.uuid}/credit_cards/#{credit_card.uuid}", %{
+          "wallet_uuid" => wallet.uuid,
+          "credit_card_uuid" => credit_card.uuid,
+          "credit_card_params" => @invalid_attrs} )
+        |> json_response(400)
+
+        assert response["error"] != %{}
+    end
+  end
+
+  describe "delete credit_card" do
+    test "deletes chosen credit_card", %{conn: conn} do
+      wallet = insert(:wallet)
+      credit_card = insert(:credit_card, %{wallet: wallet})
+
+      response =
+        delete(conn, "/api/v1/wallets/#{wallet.uuid}/credit_cards/#{credit_card.uuid}", %{"credit_card" => credit_card})
+
+      assert response.status == 204
+    end
+  end
+end
