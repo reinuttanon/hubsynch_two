b22d39f31250441d573f2eb73afe0663a3e44dc8
erin boeger <erin@hivelocity.co.jp>
2021-06-30 17:10:31 +0900

    error in prod config

--- apps/hub_cluster/config/prod.exs
+++ apps/hub_cluster/config/prod.exs
@@ -3,13 +3,23 @@ use Mix.Config
 config :libcluster,
   topologies: [
     localhost: [
-      strategy: Cluster.Strategy.Epmd,
-      config: [hosts: [String.to_atom(System.get_env("VAULT_NODE_NAME") || "vault@localhost")]]
+      strategy: Cluster.Strategy.LocalEpmd,
+      config: [
+        hosts: [
+          String.to_atom(System.get_env("VAULT_NODE_NAME") || "vault@localhost"),
+          String.to_atom(System.get_env("MNESIA_MANAGER") || "mnesia_manager@localhost")
+        ]
+      ]
     ]
   ]
 
-config :hub_cluster, :mnesia_manager, String.to_atom(System.get_env("MNESIA_MANAGER") || "mnesia_manager@localhost")]
+config :hub_cluster,
+       :mnesia_manager,
+       String.to_atom(System.get_env("MNESIA_MANAGER") || "mnesia_manager@localhost")
 
 config :hub_cluster, :mnesia_options, [
-  {:disc_copies, [String.to_atom(System.get_env("MNESIA_MANAGER") || "mnesia_manager@localhost")]}
+  {:disc_copies,
+   [String.to_atom(System.get_env("MNESIA_MANAGER") || "mnesia_manager@localhost")]},
+  {:majority, true},
+  {:ram_copies, [node()]}
 ]
