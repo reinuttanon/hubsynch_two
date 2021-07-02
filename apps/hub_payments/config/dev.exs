a41b35716bef1114ef326b9932d32667e3ddc5ae
PatOkochi <okochi@hivelocity.co.jp>
2021-07-02 12:13:18 +0900

    config changes

--- apps/hub_payments/config/dev.exs
+++ apps/hub_payments/config/dev.exs
@@ -66,16 +66,6 @@ config :hub_payments, HubPaymentsWeb.Endpoint,
     ]
   ]
 
-# Do not include metadata nor timestamps in development logs
-config :logger, :console, format: "[$level] $message\n"
-
-# Set a higher stacktrace during development. Avoid configuring such
-# in production as building large stacktraces may be expensive.
-config :phoenix, :stacktrace_depth, 20
-
-# Initialize plugs at runtime for faster development compilation
-config :phoenix, :plug_init_mode, :runtime
-
 config :hub_payments, vault_rpc: true
 
 config :hub_payments, :http_module, HubPayments.Providers.MockHttp
